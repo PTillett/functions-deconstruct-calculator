0ec3e8f8677fc9ae256f6f0ab8121e812effcd48
PTillett <pamelatillett@comcast.net>
2022-02-17 11:01:07 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const subtract = (num1, num2) => {
+    const difference = num1 - num2;
+    return difference;
+}
+const divide = (num1, num2) => {
+    const rst = num1 / num2;
+    return rst;
+}
+const multiply = (num1, num2) => {
+    const product = num1 * num2;
+    return product;
+}
+const square = (num1) => {
+    const sqr = num1 * num1
+    return sqr;
+}
+let result = subtract(11, 7)
+console.log(result);
+
+result = divide(25, 5)
+console.log(result);
+
+result = multiply(10, 2)
+console.log(result);
+
+result = square(16)
+console.log(result);
+
+//theroeiurcmauecwe test for git
